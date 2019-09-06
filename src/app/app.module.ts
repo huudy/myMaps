0799173abfc2114b63982c81698c09498125ca52
Pablo <treendaa@gmail.com>
2019-09-06 06:28:27 +0200

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { AgmCoreModule } from '@agm/core';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyAlI-RdeUPCtbXzxDMKurgLcknkjR5omto'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
