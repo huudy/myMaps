10e5b6cfe31f92ef5a68b40eea08104188d9a426
Pablo <treendaa@gmail.com>
2019-09-06 07:40:13 +0200

    map service

--- src/app/maps.service.ts
+++ src/app/maps.service.ts
@@ -1,9 +1,13 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MapsService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getLocation() {
+    return this.http.get('');
+  }
 }
